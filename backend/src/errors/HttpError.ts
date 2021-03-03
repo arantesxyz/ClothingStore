601979f52773236345fdfb140798ed33c7b39fbd
Gustavo Arantes <me@arantes.dev>
2021-03-03 03:46:14 -0300

    Added Conflict http status

--- backend/src/errors/HttpError.ts
+++ backend/src/errors/HttpError.ts
@@ -13,6 +13,7 @@ export default {
   BAD_REQUEST: craftCustomHttpError(400, 'BAD_REQUEST'),
   UNAUTHORIZED: craftCustomHttpError(401, 'UNAUTHORIZED'),
   FORBIDDEN: craftCustomHttpError(403, 'FORBIDDEN'),
+  CONFLICT: craftCustomHttpError(409, 'CONFLICT'),
 
   INTERNAL_SERVER_ERROR: craftCustomHttpError(500, 'INTERNAL_SERVER_ERROR')
 }
