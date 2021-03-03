3312e513d98a8c8b724598f943d6bae9b3ca7a6d
Gustavo Arantes <me@arantes.dev>
2021-03-03 03:51:16 -0300

    Initialize routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,10 @@
 import { Router } from 'express'
+import { UsersRoute } from './modules/user'
+import { AuthRoute } from './modules/auth'
 
 const mainRoute = Router()
+
+mainRoute.use('/auth', AuthRoute)
+mainRoute.use('/users', UsersRoute)
 
 export default mainRoute
