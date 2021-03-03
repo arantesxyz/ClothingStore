949b5329e25862f3924ecd8278d2b8fc94e378c5
Gustavo Arantes <me@arantes.dev>
2021-03-02 21:37:33 -0300

    Add error handlers

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,14 @@
 import express from 'express'
+import 'express-async-errors'
+
 import routes from './routes'
+import HttpErrorHandler from './middlewares/HttpErrorHandler'
 
 const app = express()
 
 app.use(express.json())
 app.use(routes)
+
+app.use(HttpErrorHandler)
 
 export default app
